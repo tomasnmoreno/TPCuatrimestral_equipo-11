0c1274c748204370222640a8b8bf67e510eac4e8
tomasnmoreno <tomasnehuenmoreno@gmail.com>
2024-05-30 03:39:49 -0300

    MasterPage + Home (hay que revisarle algunas cosas)

---
+++ AppClinicaMedica/js/app.js
@@ -0,0 +1,24 @@
+﻿//document.addEventListener("DOMContentLoaded", () => {
+
+//    //selec de elementosdel DOM//
+//    const toggleButton = document.querySelector(".navbar__toggle-btn");
+//    const mobileMenu = document.querySelector(".navbar__mobile-menu");
+
+//    //console.log(mobileMenu)
+
+//    const toggleMenu = () => {
+//        mobileMenu.style.display =
+//            mobileMenu.style.display === "none" || mobileMenu.style.display === ""
+//                ? "flex"
+//                : "none";
+//    };
+
+//    const hideMenuResize = () => {
+//        mobileMenu.style.display = "none"
+//    };
+
+//    toggleButton.addEventListener("click", toggleMenu);
+
+//    window.addEventListener("resize", hideMenuResize);
+
+//});
